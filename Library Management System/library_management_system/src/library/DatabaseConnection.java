8372fdf2ed718efa1910943d0ccb276e6ba5a570
Tony Christopher J <113699865+Tonychris115@users.noreply.github.com>
2025-07-28 19:44:50 +0530

    Update DatabaseConnection.java

--- Library Management System/library_management_system/src/library/DatabaseConnection.java
+++ Library Management System/library_management_system/src/library/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DatabaseConnection {
 	private static final String url = "jdbc:mysql://localhost:3306/librarydb";
 	private static final String username = "root";
-	private static final String password = "Tonychris454@";
+	private static final String password = "--------";
 	
 	public static Connection getConnection()throws SQLException {
 		return DriverManager.getConnection(url, username, password);
